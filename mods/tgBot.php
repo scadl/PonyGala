093b623a786078587664e5325e65ef2a9cce93b4
Alexandr <scad.luncher@gmail.com>
2022-08-11 17:33:17 +0300

    new tgBot function simple art selection

--- mods/tgBot.php
+++ mods/tgBot.php
@@ -6,163 +6,254 @@ require '../db_init.php';
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+// About Telegrma bot system
+// https://core.telegram.org/bots                   -- Get started and Demo
+// https://core.telegram.org/bots/api               -- All API
+// https://core.telegram.org/bots/faq               -- Bot FAQ
+// https://core.telegram.org/bots/samples/hellobot  -- Simple bot
+
 define('BOT_TOKEN', $tg_bot_secret);
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
-?>
 
-<style>
-form{
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    background:#ccc; 
-    padding: 5px; 
-    display:inline-block
-}
-</style>
 
-<form method="POST" action="<?php print(API_URL); ?>" >
-    <input type="hidden" name="method" value="setWebhook">
-    URL <br> 
-    <input type="text" name="url"> <br>
-    certificate <br>
-    <input type="file" name="certificate"> <br>
-    ip_address <br>
-    <input type="text" name="ip_address	"> <br>
-    max_connections <br>
-    <input type="number" name="max_connections"> <br>
-    allowed_updates <br>
-    <select name="allowed_updates" multiple> 
-        <option value="message">message</option>
-        <option value="edited_channel_post">edited_channel_post</option>
-        <option value="callback_query">callback_query</option>
-    </select> <br>
-    <input type="checkbox" name="drop_pending_updates"> drop_pending_updates <br>
-    <input type="submit" value="set WebHook">
-</form>
-<form method="POST" action="<?php print(API_URL); ?>" >
-    <input type="hidden" name="method" value="setWebhook">
-    <input type="hidden" name="url" value="">
-    <input type="submit" value="remove WebHook">
-</form>
-
-<?php
 function simpleRequest($method, $params){
+        
+    //print(API_URL."<br>");
+
     // Get the messages sended to bot throught the last 24 hours
     // getUpdates is a poll-type method (https://core.telegram.org/bots/api#getupdates)
     $params["method"] = $method;
     $handle = curl_init(API_URL);
     curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
-    curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+    curl_setopt($handle, CURLOPT_TIMEOUT, 300);
     curl_setopt($handle, CURLOPT_POST, true);
+    curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);                // Curl error 60, SSL certificate issue: self signed certificate in certificate chain
     curl_setopt($handle, CURLOPT_POSTFIELDS, json_encode($params));
     curl_setopt($handle, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+        
+    //print($handle."<br>");
+    
+
+    //$httpCode = curl_getinfo($handle , CURLINFO_HTTP_CODE); // this results 0 every time
+    $response = curl_exec($handle);
+
+    //if ($response === false) 
+    //    $response = curl_error($handle);
+
+    //echo 'Error: ' . stripslashes($response);
 
     // An 'Update' object (https://core.telegram.org/bots/api#update)
     // Carry all data about users activity, captured by bot
-    return json_decode(curl_exec($handle), false);
+    return json_decode($response, false);
+}
+
+function telegram_emoji($utf8emoji) {
+    preg_replace_callback(
+        '@\\\x([0-9a-fA-F]{2})@x',
+        function ($captures) {
+            return chr(hexdec($captures[1]));
+        },
+        $utf8emoji
+    );
+
+    return $utf8emoji;
 }
 
 function sendResponseWithArt($artCatID, $tgChatID){
 
     global $link;
+    $artOk = false;
 
-    $rq = "SELECT * FROM arts_pub WHERE category=".$artCatID." ORDER BY rand() LIMIT 1";
-    $sql = mysqli_query($link, $rq);
-    while ($row = mysqli_fetch_array($sql, MYSQLI_ASSOC)) {
-    simpleRequest(
-        'sendPhoto', 
-        array(
-            'chat_id' => $tgChatID,
-            'parse_mode' => 'MarkdownV2',
-            'photo' => $row['file_name'],
-            'caption' => '*'.$row['title'].'*' . ' by _'.$row['author'].'_'
-        )
-    );
+    // Try to send image until it actualy loaded from source
+    while(!$artOk){
+        $rq = "SELECT * FROM arts_pub WHERE category=".$artCatID." ORDER BY rand() LIMIT 1";
+        $sql = mysqli_query($link, $rq);
+        while ($row = mysqli_fetch_array($sql, MYSQLI_ASSOC)) {
+
+            $resultPh = simpleRequest(
+            'sendPhoto', 
+                array(
+                    'chat_id' => $tgChatID,
+                    'parse_mode' => 'MarkdownV2',
+                    'photo' => $row['file_name'],
+                    'caption' => '[*'.$row['title'].'*]('.$row['da_page'].')' . ' by _'.$row['author'].'_'
+                )
+            );
+
+            // Ask Telegram, if image sent wtih provided link.
+            // Has to relay on telegram because of connection problems in my region
+            $artOk = $resultPh->ok;
+            
+            // Debug - send respones body
+            // simpleRequest('sendMessage', array('chat_id' => $tgChatID, 'text' => $resultPh));
+        }
+    }
+
     simpleRequest(
         'sendMessage', 
         array(
             'chat_id' => $tgChatID,
-            'text' => 'Это случайный арт из выбарнной категории.  Тебе понравилось?',
+            'parse_mode' => 'MarkdownV2',
+            'text' => 'Это случайный арт из выбарнной категории\.'.PHP_EOL.'Тебе понравилось\?',
             'reply_markup' => array(
-                'keyboard' => array(array('Отличный арт', 'Не не пойдет')),
+                'keyboard' => array(array(' Отличный арт', 'Не не пойдет')),
                 'one_time_keyboard' => true,
                 'resize_keyboard'=> true
             )
         )
     );
-    }
 }
 
-$response = simpleRequest('getUpdates',array());
+function sendLastSelection($tgChatID){
 
-print('<pre>');
-print_r('isOk?' . $response->ok.'<br>');
-foreach($response->result as $key => $val){
+    global $link;
+
+    
+
+    $publication = '';
+    $sqlp = mysqli_query($link, "SELECT `file_name`, addate FROM arts_pub GROUP BY addate ORDER BY STR_TO_DATE(addate, '%e-%m-%Y') DESC LIMIT 1");                
+    while ($rowp = mysqli_fetch_array($sqlp, MYSQLI_ASSOC)) {
+        $publication = $rowp['addate'];                
+    } 
+
+    $artOk = false;
+    while(!$artOk){
+
+        $images = array();
+        $rq = "SELECT * FROM arts_pub WHERE addate='".$publication."' ORDER BY rand() LIMIT 5";
+        $sql = mysqli_query($link, $rq);
+        while ($row = mysqli_fetch_array($sql, MYSQLI_ASSOC)) {
+
+            $images[] = array(
+                'type'=>'photo',
+                'media'=> $row['file_name'],
+                'caption'=> '"'.$row['title'].'" by '.$row['author']
+            );
+
+        }
+
+
+        $resultPh = simpleRequest(
+            'sendMediaGroup', 
+            array(
+                'chat_id' => $tgChatID,
+                'media' => json_encode($images)
+            )
+        );
+        
+        $artOk = $resultPh->ok;
+    }
+
+    $date = new DateTime($publication);
+    simpleRequest(
+        'sendMessage', 
+        array(
+            'chat_id' => $tgChatID,
+            'parse_mode' => 'MarkdownV2',
+            'text' => 'Это случайные арты от '.date_format($date, "d M Y").'\.'.PHP_EOL.'Тебе понравилось\?',
+            'reply_markup' => array(
+                'keyboard' => array(array('Да, давай еще!', 'Не, давай другие')),
+                'one_time_keyboard' => true,
+                'resize_keyboard'=> true
+            )
+        )
+    );
+}
+
+
+function webhookProcess($response){
+
+    global $link;
+
+    $val = $response;
+    $categories = array();
+    $categ_ids = array();
+    $cat_num=0;
+
+        $rq = "SELECT * FROM categories WHERE cat_id<>1";
+        $sql = mysqli_query($link, $rq);
+        while ($row = mysqli_fetch_array($sql, MYSQLI_ASSOC)) {
+            $categories[] = $row['cat_name'];   
+            $categ_ids[] = $row['cat_id'];
+            $cat_num++;     
+        }
+
+        for($i=0; $i<$cat_num; $i+=2){
+            $cat_format[] = array($categories[$i], $categories[$i+1]);
+        }
+
+        $cat_buttons = array(
+            'keyboard' => $cat_format,
+            'one_time_keyboard' => true,
+            'resize_keyboard'=> true
+        );
 
     if(property_exists($val, 'message')){
         
         print_r($val->update_id .' '. $val->message->text.'<br>');
 
         // Send a response to user, only if this message is new
-        if(isset($_COOKIE['tg_last_update_id']) && $val->update_id > $_COOKIE['tg_last_update_id']){
+        //if(isset($_COOKIE['tg_last_update_id']) && $val->update_id > $_COOKIE['tg_last_update_id']){
 
-            if (strpos($val->message->text, "/start") === 0) {
-                simpleRequest('sendMessage', array(
+            if (strpos($val->message->text, "/start") === 0 OR strpos($val->message->text, "Не, давай другие") === 0) {
+                $resultPh = simpleRequest('sendMessage', array(
                     'chat_id' => $val->message->chat->id,
                     'text' => "Привет! Какие арты тебе нравятся?",
-                    'reply_markup' => array(
-                            'keyboard' => array(array('Поняшки', 'Портреты'), array('Пейзажики', 'Машинки')),
-                            'one_time_keyboard' => true,
-                            'resize_keyboard'=> true
-                        )
+                    'reply_markup' => $cat_buttons
                 ));
-            }
-            if(strpos($val->message->text, "Поняшки") === 0){
-                sendResponseWithArt(3, $val->message->chat->id);
-            }
-            if(strpos($val->message->text, "Портреты") === 0){
-                sendResponseWithArt(23, $val->message->chat->id);
-            }
-            if(strpos($val->message->text, "Пейзажики") === 0){
-                sendResponseWithArt(16, $val->message->chat->id);
-            }
-            if(strpos($val->message->text, "Машинки") === 0){
-                sendResponseWithArt(24, $val->message->chat->id);
-            }
 
+                 // Debug - send respones body
+                //simpleRequest('sendMessage', array('chat_id' => $val->message->chat->id, 'text' => $resultPh  ));
+            }
+            if (strpos($val->message->text, "/last") === 0 OR strpos($val->message->text, "Да, давай еще!") === 0 ) {
+                sendLastSelection($val->message->chat->id);
+            }
+            foreach($categories as $cKey => $cVal){
+                if(strpos($val->message->text, $cVal) === 0){
+                    sendResponseWithArt($categ_ids[$cKey], $val->message->chat->id);
+                }
+            }          
             if(strpos($val->message->text, "Отличный арт") === 0){
                 simpleRequest('sendMessage', array(
                     'chat_id' => $val->message->chat->id,
-                    'text' => "Мы рады что тебе понравилось. Может быть хочешь еще?",
-                    'reply_markup' => array(
-                            'keyboard' => array(array('Да', 'Нет')),
-                            'one_time_keyboard' => true,
-                            'resize_keyboard'=> true
-                        )
+                    'text' => 'Мы рады что тебе понравилось.'.PHP_EOL.'Может быть хочешь еще один? '.PHP_EOL.'Выбирай категорию:',
+                    'reply_markup' => $cat_buttons
                 ));
-            }
-
-            if(strpos($val->message->text, "Да") === 0){
-                
-            }
-
-            if(strpos($val->message->text, "Нет") === 0){
-                
             }
 
             if(strpos($val->message->text, "Не не пойдет") === 0){
                 simpleRequest('sendMessage', array(
                     'chat_id' => $val->message->chat->id,
-                    'text' => "Жаль что тебе не нравится наш выбор. Давай попробуем другую категорию:",
-                    'reply_markup' => array(
-                        'keyboard' => array(array('Поняшки', 'Портреты'), array('Пейзажики', 'Машинки')),
+                    'text' => 'Нам жаль что тебе не нравится наш выбор. '.PHP_EOL.'Давай попробуем другую категорию:',
+                    'reply_markup' => $cat_buttons
+                ));
+            }
+
+            if(strpos($val->message->text, "/help") === 0){
+                simpleRequest('sendMessage', array(
+                    'chat_id' => $val->message->chat->id,
+                    'text' => 'Наш бот пока умеет немного: '.PHP_EOL.
+                        '/start - Перезапуск и выбор категории'.PHP_EOL.
+                        '/last - Случайные 5 аров из последнней подборки'.PHP_EOL.
+                        '/help - Это справочное сообщение'.PHP_EOL.
+                        '/settings - Настройки бота'.PHP_EOL,
+                ));
+            }
+
+            if(strpos($val->message->text, "/settings") === 0){
+                simpleRequest('sendMessage', array(
+                    'chat_id' => $val->message->chat->id,
+                    'text' => 'Здесь будут настройки бота',
+                    'reply_markup' =>  array(
+                        'keyboard' => array(array('/start')),
                         'one_time_keyboard' => true,
                         'resize_keyboard'=> true
                     )
                 ));
             }
             
-        }
+        //}
 
     }
 
@@ -171,10 +262,31 @@ foreach($response->result as $key => $val){
     }
 
     // Remeber an id of last repalyed message
-    SetCookie("tg_last_update_id", $val->update_id);
+    SetCookie("tg_last_update_id", $val->update_id);    
+
+    print('<pre>');
+    print_r($response);
+    print('</pre>');
 }
-print_r($response->result);
-print('</pre>');
+
+//Check if curl installed and working
+//print( "Function exist:" . function_exists('curl_version') ."<br>" );
+
+$content = file_get_contents("php://input");    // will make php to listen for incoming request
+$update = json_decode($content, false);         // start process incoming data
+
+// Dump incoming data to file
+//$req_dump = print_r( $content, true );
+//$fp = file_put_contents( 'webhook_request.log', $req_dump );
+
+if (!$update) {
+  // receive wrong update, must not happen
+  exit;
+}
+
+if (property_exists($update, "message")) {
+    webhookProcess($update);
+}
 
 // Other way to get update is supplay a bot a url so it will send all the messages to it.
 // This is a webHook mechanism: If bot recieves a message, it send an HTTSP request to designited url.
